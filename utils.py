35eca209c76ebd1412b915011ace462ae4e4c037
AngusG <premiumdriedfoods@gmail.com>
2020-01-13 17:01:18 -0500

    crop with only two inliers

---
+++ utils.py
@@ -0,0 +1,17 @@
+
+
+def line_intersection(line1, line2):
+    xdiff = (line1[0][0] - line1[1][0], line2[0][0] - line2[1][0])
+    ydiff = (line1[0][1] - line1[1][1], line2[0][1] - line2[1][1])
+
+    def det(a, b):
+        return a[0] * b[1] - a[1] * b[0]
+
+    div = det(xdiff, ydiff)
+    if div == 0:
+        return None
+
+    d = (det(*line1), det(*line2))
+    x = det(d, xdiff) / div
+    y = det(d, ydiff) / div
+    return x, y
