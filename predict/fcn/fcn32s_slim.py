0cb75acc856a45add4b9c3f023026e4e4383fc21
AngusG <premiumdriedfoods@gmail.com>
2020-03-21 19:33:05 -0400

    bugfix

--- predict/fcn/fcn32s_slim.py
+++ predict/fcn/fcn32s_slim.py
@@ -37,20 +37,20 @@ class FCN32slim(nn.Module):
         )
 
     def __init__(self, n_class=21):
-        super(FCN32s, self).__init__()
+        super(FCN32slim, self).__init__()
         # conv1
         self.conv1_1 = nn.Conv2d(3, 64, 3, padding=100)
         self.relu1_1 = nn.ReLU(inplace=True)
         #self.conv1_2 = nn.Conv2d(64, 64, 3, padding=1)
         #self.relu1_2 = nn.ReLU(inplace=True)
-        #self.pool1 = nn.MaxPool2d(2, stride=2, ceil_mode=True)  # 1/2
+        self.pool1 = nn.MaxPool2d(2, stride=2, ceil_mode=True)  # 1/2
 
         # conv2
         self.conv2_1 = nn.Conv2d(64, 128, 3, padding=1)
         self.relu2_1 = nn.ReLU(inplace=True)
         #self.conv2_2 = nn.Conv2d(128, 128, 3, padding=1)
         #self.relu2_2 = nn.ReLU(inplace=True)
-        #self.pool2 = nn.MaxPool2d(2, stride=2, ceil_mode=True)  # 1/4
+        self.pool2 = nn.MaxPool2d(2, stride=2, ceil_mode=True)  # 1/4
 
         # conv3
         self.conv3_1 = nn.Conv2d(128, 256, 3, padding=1)
@@ -60,8 +60,9 @@ class FCN32slim(nn.Module):
         self.relu3_2 = nn.ReLU(inplace=True)
         self.conv3_3 = nn.Conv2d(256, 256, 3, padding=1)
         self.relu3_3 = nn.ReLU(inplace=True)
-        self.pool3 = nn.MaxPool2d(2, stride=2, ceil_mode=True)  # 1/8
         '''
+        self.pool3 = nn.MaxPool2d(2, stride=2, ceil_mode=True)  # 1/8
+
         # conv4
         self.conv4_1 = nn.Conv2d(256, 512, 3, padding=1)
         self.relu4_1 = nn.ReLU(inplace=True)
@@ -70,8 +71,9 @@ class FCN32slim(nn.Module):
         self.relu4_2 = nn.ReLU(inplace=True)
         self.conv4_3 = nn.Conv2d(512, 512, 3, padding=1)
         self.relu4_3 = nn.ReLU(inplace=True)
-        self.pool4 = nn.MaxPool2d(2, stride=2, ceil_mode=True)  # 1/16
         '''
+        self.pool4 = nn.MaxPool2d(2, stride=2, ceil_mode=True)  # 1/16
+
         # conv5
         self.conv5_1 = nn.Conv2d(512, 512, 3, padding=1)
         self.relu5_1 = nn.ReLU(inplace=True)
@@ -80,8 +82,8 @@ class FCN32slim(nn.Module):
         self.relu5_2 = nn.ReLU(inplace=True)
         self.conv5_3 = nn.Conv2d(512, 512, 3, padding=1)
         self.relu5_3 = nn.ReLU(inplace=True)
-        self.pool5 = nn.MaxPool2d(2, stride=2, ceil_mode=True)  # 1/32
         '''
+        self.pool5 = nn.MaxPool2d(2, stride=2, ceil_mode=True)  # 1/32
         # fc6
         self.fc6 = nn.Conv2d(512, 2048, 7)
         self.relu6 = nn.ReLU(inplace=True)
@@ -96,12 +98,13 @@ class FCN32slim(nn.Module):
         self.upscore = nn.ConvTranspose2d(n_class, n_class, 64, stride=32,
                                           bias=False)
 
-        #self._initialize_weights()
+        self._initialize_weights()
 
     def _initialize_weights(self):
         for m in self.modules():
             if isinstance(m, nn.Conv2d):
-                m.weight.data.zero_()
+                #m.weight.data.zero_()
+                nn.init.kaiming_normal_(m.weight, mode='fan_out', nonlinearity='relu')
                 if m.bias is not None:
                     m.bias.data.zero_()
             if isinstance(m, nn.ConvTranspose2d):
@@ -113,26 +116,26 @@ class FCN32slim(nn.Module):
     def forward(self, x):
         h = x
         h = self.relu1_1(self.conv1_1(h))
-        h = self.relu1_2(self.conv1_2(h))
+        #h = self.relu1_2(self.conv1_2(h))
         h = self.pool1(h)
 
         h = self.relu2_1(self.conv2_1(h))
-        h = self.relu2_2(self.conv2_2(h))
+        #h = self.relu2_2(self.conv2_2(h))
         h = self.pool2(h)
 
         h = self.relu3_1(self.conv3_1(h))
-        h = self.relu3_2(self.conv3_2(h))
-        h = self.relu3_3(self.conv3_3(h))
+        #h = self.relu3_2(self.conv3_2(h))
+        #h = self.relu3_3(self.conv3_3(h))
         h = self.pool3(h)
 
         h = self.relu4_1(self.conv4_1(h))
-        h = self.relu4_2(self.conv4_2(h))
-        h = self.relu4_3(self.conv4_3(h))
+        #h = self.relu4_2(self.conv4_2(h))
+        #h = self.relu4_3(self.conv4_3(h))
         h = self.pool4(h)
 
         h = self.relu5_1(self.conv5_1(h))
-        h = self.relu5_2(self.conv5_2(h))
-        h = self.relu5_3(self.conv5_3(h))
+        #h = self.relu5_2(self.conv5_2(h))
+        #h = self.relu5_3(self.conv5_3(h))
         h = self.pool5(h)
 
         h = self.relu6(self.fc6(h))
